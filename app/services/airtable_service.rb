a751e80f3ec7e5027cfa3465f402606409086244
Arav Narula <arav@hackclub.com>
2025-12-23 19:04:28 -0500

    Make the date that the person signed up actually be right

--- app/services/airtable_service.rb
+++ app/services/airtable_service.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class AirtableService
   BASE_URL = "https://api.airtable.com/v0".freeze
   BASE_ID = "appG8V9462X6QDhpZ".freeze
@@ -12,7 +14,7 @@ class AirtableService
       req.body = {
         fields: {
           email: email,
-          created_at: Time.current.strftime("%Y-%m-%d"),
+          created_at: Time.now,
           origin_ip: origin_ip
         }
       }.to_json
