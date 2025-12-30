b69a342e987fdd941f1e1f68dd5b921d09cfecbc
Arav Narula <arav@hackclub.com>
2025-12-30 18:38:14 -0500

    Make project submission on it's own page

--- app/frontend/pages/projects/new.tsx
+++ app/frontend/pages/projects/new.tsx
@@ -1,6 +1,5 @@
-import { Link } from "@inertiajs/react";
-import Sidebar from "@/components/sidebar";
-import ProjectList from "@/components/Projects/ProjectList";
+import ProjectCreate from "@/components/Projects/ProjectCreate";
+import Sidebar from "@/components/Sidebar";
 
 interface Project {
   id: number;
@@ -25,28 +24,16 @@ interface Project {
   user_id: number;
 }
 
-export default function Index({ projects }: { projects: Project[] }) {
-  console.log(projects);
+export default function NewProject() {
   return (
-    
     <div className="flex bg-gray-200 min-h-screen">
       <Sidebar />
-    
       <main className="flex-1 lg:ml-64 p-6">
-        <h1 className="text-xl font-bold mt-8 mb-4">Projects</h1>
-        <Link href="/projects/new" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create Project</Link>
-      {projects.length === 0 && (
-        <>
-          <h1 className="text-center text-2xl font-bold text-gray-800">
-            Make your first project!
-          </h1>
-        </>
-      )}
-    
-
-
-      <h1 className="text-xl font-bold mt-8 mb-4">Your Projects</h1>
-      <ProjectList projects={projects} />
+  
+            <h1 className="text-center text-2xl font-bold text-gray-800">
+          Make your first project!
+        </h1>
+        <ProjectCreate />
       </main>
     </div>
   );
