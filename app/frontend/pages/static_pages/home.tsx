49d3552211621f0eecc110c1f40987d54087d92d
Arav Narula <arav@hackclub.com>
2025-12-30 21:35:44 -0500

    same bg colour on all pages that exist rn

--- app/frontend/pages/static_pages/home.tsx
+++ app/frontend/pages/static_pages/home.tsx
@@ -11,7 +11,7 @@ export default function Home({
   current_user: any;
 }) {
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex min-h-screen bg-gray-200">
       <Sidebar />
       <main className="flex-1 lg:ml-64 p-6">
         <p className="text-2xl font-bold">hey {current_user.username}!</p>
