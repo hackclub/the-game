81144fe5a00348d80a53a0626c9a792c8b05f4f5
Arav Narula <arav@hackclub.com>
2026-01-02 13:40:00 -0500

    Add conditonal labels for review status and shipped projects

--- app/frontend/components/Projects/ProjectList.tsx
+++ app/frontend/components/Projects/ProjectList.tsx
@@ -1,13 +1,30 @@
-import { Link } from "@inertiajs/react";
-
+import { App, Link } from "@inertiajs/react";
 
 export default function ProjectList({ projects }: { projects: Project[] }) {
+  const isReviewed = (review_status: string) => {
+    console.log(review_status);
+    if (review_status === "approved") {
+      return "Approved";
+    } else if (review_status === "pending") {
+      return "Pending";
+    } else if (review_status === "rejected") {
+      return "Rejected";
+    }
+  };
+
+  const isShipped = (shipStatus: string) => {
+    if (shipStatus === "shipped") {
+      return true
+    }
+    return false;
+  };
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {projects.map((project) => (
         <div key={project.id} className="p-4 border rounded-lg shadow-sm">
           <h2 className="text-lg font-semibold">{project.title}</h2>
-          <span className="bg-amber-200"> Review: {project.review_status}</span>
+          <span className=""> {isReviewed(project.review_status)}</span>
           <p className="text-gray-600 mt-4">{project.desc}</p>
           <div className="mt-6 flex gap-2">
             {project.demo_link && (
@@ -32,8 +49,12 @@ export default function ProjectList({ projects }: { projects: Project[] }) {
             >
               Edit
             </Link>
-    
-            <span className="text-gray-500">{project.approved}</span>
+
+            <span
+              className={`text-${isShipped(project.approved) ? "green" : "red"}-500`}
+            >
+              {isShipped(project.approved) ? "Shipped!" : "Not Shipped!"}
+            </span>
             <br />
           </div>
         </div>
