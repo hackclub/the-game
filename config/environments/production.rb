ad3b1597389522517d4fc10e278df5ceb2dd6751
polypixeldev <me@sfernandez.dev>
2026-01-16 17:37:36 -0500

    Set default url host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
+  Rails.application.routes.default_url_options[:host] = ENV[:LIVE_URL_HOST]
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
