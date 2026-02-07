923bde281c4ab3895f67d4662526854e2862f0f0
polypixeldev <me@sfernandez.dev>
2026-02-06 23:01:21 -0500

    Fix constraint routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  constraints AdminConstraint do
+  constraints AdminConstraint.new do
     mount Sidekiq::Web => "/sidekiq"
     mount Blazer::Engine, at: "blazer"
   end
