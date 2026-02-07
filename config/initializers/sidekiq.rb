50530e224752da3638f126c81f2aa990d76076ab
polypixeldev <me@sfernandez.dev>
2026-02-06 23:10:07 -0500

    Remove namespace from sidekiq redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@ return unless Rails.env.production?
 redis_url = ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" }
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url, namespace: "sidekiq" }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url, namespace: "sidekiq" }
+  config.redis = { url: redis_url }
 end
