0e19cd5135bf0d62ddc6509e7e3034cc153462ab
polypixeldev <me@sfernandez.dev>
2026-02-07 23:45:47 -0500

    Don't configure sidekiq in dev

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -1,3 +1,5 @@
-Sidekiq::Cron.configure do |config|
-  config.cron_poll_interval = 30
+unless Rails.env.development?
+  Sidekiq::Cron.configure do |config|
+    config.cron_poll_interval = 30
+  end
 end
