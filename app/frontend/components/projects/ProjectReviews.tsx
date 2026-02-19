deabb906640dbdd59592f53bc30b5dc8ef62e6e2
polypixeldev <me@sfernandez.dev>
2026-02-19 18:00:31 -0500

    Fix up new review styling

--- app/frontend/components/projects/ProjectReviews.tsx
+++ app/frontend/components/projects/ProjectReviews.tsx
@@ -32,12 +32,14 @@ export default function ProjectReviews({
         Reviews
       </h2>
       {project.aasm_state === "pending" ? (
-        <p>Ship your project so that we can review it!</p>
+        <p className="text-xl">Ship your project so that we can review it!</p>
       ) : (
         <>
           <div className="flex flex-col gap-5">
             {project.reviews.length == 0 ? (
-              <p>We haven't reviewed your project yet - give us some time!</p>
+              <p className="text-xl">
+                We haven't reviewed your project yet - give us some time!
+              </p>
             ) : (
               project.reviews.map((review) => (
                 <div className="flex gap-3" key={review.id}>
@@ -75,97 +77,96 @@ export default function ProjectReviews({
             )}
           </div>
 
-          {props.user.role === "reviewer" ||
-            (props.user.role === "admin" && (
-              <form
-                className="mt-6 flex w-full flex-col gap-4 px-4 md:px-0"
-                onSubmit={submitReview}
-              >
-                <div className="flex items-center gap-4">
-                  <select
-                    className="border-[#cacaca] bg-[#d9d9d9] py-2 pr-12 pl-6 text-lg outline-none"
-                    value={data.review_type}
-                    onChange={(e) => {
-                      setData("review_type", e.target.value);
-                    }}
-                  >
-                    <option value="comment">Comment</option>
-                    {project.aasm_state === "submitted" && (
-                      <>
-                        <option value="rejection">Rejection</option>
-                        <option value="approval">Approval</option>
-                      </>
-                    )}
-                  </select>
-                  {data.review_type === "comment" && (
-                    <div className="flex items-center gap-2">
-                      <input
-                        type="checkbox"
-                        checked={adminOnly}
-                        onChange={(e) => {
-                          if (adminOnly) {
-                            setData("content", data.admin_content);
-                            setData("admin_content", "");
-                          } else {
-                            setData("admin_content", data.content);
-                            setData("content", "");
-                          }
-                          setAdminOnly(e.target.checked);
-                        }}
-                      />
-                      <label>Admin only?</label>
-                    </div>
+          {(props.user.role === "reviewer" || props.user.role === "admin") && (
+            <form
+              className="mt-6 flex w-full flex-col gap-4 px-4 md:px-0"
+              onSubmit={submitReview}
+            >
+              <div className="flex items-center gap-4">
+                <select
+                  className="border-[#cacaca] bg-[#d9d9d9] py-2 pr-12 pl-6 text-lg outline-none"
+                  value={data.review_type}
+                  onChange={(e) => {
+                    setData("review_type", e.target.value);
+                  }}
+                >
+                  <option value="comment">Comment</option>
+                  {project.aasm_state === "submitted" && (
+                    <>
+                      <option value="rejection">Rejection</option>
+                      <option value="approval">Approval</option>
+                    </>
                   )}
-                  {data.review_type === "approval" && (
-                    <div className="flex items-center gap-2">
-                      <input
-                        type="number"
-                        value={data.approved_hours}
-                        className="border-[#cacaca] bg-[#d9d9d9] px-4 py-2 text-xl outline-none"
-                        onChange={(e) => {
-                          setData("approved_hours", Number(e.target.value));
-                        }}
-                      />
-                      <label>hours</label>
-                    </div>
-                  )}
-                </div>
+                </select>
+                {data.review_type === "comment" && (
+                  <div className="flex items-center gap-2">
+                    <input
+                      type="checkbox"
+                      checked={adminOnly}
+                      onChange={(e) => {
+                        if (adminOnly) {
+                          setData("content", data.admin_content);
+                          setData("admin_content", "");
+                        } else {
+                          setData("admin_content", data.content);
+                          setData("content", "");
+                        }
+                        setAdminOnly(e.target.checked);
+                      }}
+                    />
+                    <label className="text-lg">Internal?</label>
+                  </div>
+                )}
+                {data.review_type === "approval" && (
+                  <div className="flex items-center gap-2">
+                    <input
+                      type="number"
+                      value={data.approved_hours}
+                      className="border-[#cacaca] bg-[#d9d9d9] px-4 py-2 text-xl outline-none"
+                      onChange={(e) => {
+                        setData("approved_hours", Number(e.target.value));
+                      }}
+                    />
+                    <label className="text-lg">hours</label>
+                  </div>
+                )}
+              </div>
+              <textarea
+                className="h-[117px] resize-none border-[#cacaca] bg-[#d9d9d9] p-4 text-xl outline-none"
+                value={
+                  data.review_type === "comment" && adminOnly
+                    ? data.admin_content
+                    : data.content
+                }
+                onChange={(e) => {
+                  if (data.review_type === "comment" && adminOnly) {
+                    setData("admin_content", e.target.value);
+                  } else {
+                    setData("content", e.target.value);
+                  }
+                }}
+                required={data.review_type !== "comment"}
+                placeholder={`Add your comment here - this will ${adminOnly && data.review_type === "comment" ? "only be visible to admins and reviewers" : "be shown to the author"}`}
+              />
+              {data.review_type !== "comment" && (
                 <textarea
                   className="h-[117px] resize-none border-[#cacaca] bg-[#d9d9d9] p-4 text-xl outline-none"
-                  value={
-                    data.review_type === "comment" && adminOnly
-                      ? data.admin_content
-                      : data.content
-                  }
+                  value={data.admin_content}
                   onChange={(e) => {
-                    if (data.review_type === "comment" && adminOnly) {
-                      setData("admin_content", e.target.value);
-                    } else {
-                      setData("content", e.target.value);
-                    }
+                    setData("admin_content", e.target.value);
                   }}
-                  required={data.review_type !== "comment"}
-                  placeholder={`Add your comment here - this will ${adminOnly && data.review_type === "comment" ? "only be visible to admins" : "be shown to the author"}`}
+                  required
+                  placeholder="Justify this review - this is only shown to admins and reviewers"
                 />
-                {data.review_type !== "comment" && (
-                  <textarea
-                    className="h-[117px] resize-none border-[#cacaca] bg-[#d9d9d9] p-4 text-xl outline-none"
-                    value={data.admin_content}
-                    onChange={(e) => {
-                      setData("admin_content", e.target.value);
-                    }}
-                    required
-                    placeholder="Justify this review - this is only shown to admins"
-                  />
-                )}
-                <button
-                  className="cursor-pointer bg-black px-6 py-2 text-lg font-bold text-white hover:bg-gray-800"
-                  disabled={processing}
-                >
-                  Add review
-                </button>
-              </form>
-            ))}
+              )}
+              <button
+                className="cursor-pointer bg-black px-6 py-2 text-lg font-bold text-white hover:bg-gray-800"
+                disabled={processing}
+              >
+                Add review
+              </button>
+            </form>
+          )}
         </>
       )}
     </div>
