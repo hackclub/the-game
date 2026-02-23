67ebb36745617a704097f79869411bbc3726ab22
polypixeldev <me@sfernandez.dev>
2026-02-23 18:11:26 -0500

    Add alltime leaderboard

--- app/frontend/pages/review/index.tsx
+++ app/frontend/pages/review/index.tsx
@@ -4,11 +4,19 @@ import ProjectCard from "@/components/projects/ProjectCard";
 
 interface Props {
   queue: (Project & { username: string })[];
-  leaderboard: { id: number; name: string; count: number }[];
+  week_leaderboard: { id: number; name: string; count: number }[];
+  alltime_leaderboard: { id: number; name: string; count: number }[];
 }
 
-export default function Review({ queue, leaderboard }: Props) {
-  const sorted = [...leaderboard].sort((a, b) => b.count - a.count);
+export default function Review({
+  queue,
+  week_leaderboard,
+  alltime_leaderboard,
+}: Props) {
+  const weekSorted = [...week_leaderboard].sort((a, b) => b.count - a.count);
+  const alltimeSorted = [...alltime_leaderboard].sort(
+    (a, b) => b.count - a.count,
+  );
 
   return (
     <Layout>
@@ -37,19 +45,37 @@ export default function Review({ queue, leaderboard }: Props) {
             </p>
           </div>
 
-          <div className="flex max-w-xl flex-col gap-3">
-            {sorted.map((user, index) => (
-              <div
-                className={`flex items-center gap-3 rounded-md ${index == 0 ? "bg-yellow-500" : index == 1 ? "bg-gray-400" : index == 2 ? "bg-amber-600" : "bg-gray-200"} p-4`}
-              >
-                <p className="pr-5 pl-2 font-bold">{index + 1}</p>
-                <p className="font-bold">{user.name}</p>
-                <div className="grow"></div>
-                <p>
-                  {user.count} review{user.count > 1 && "s"}
-                </p>
-              </div>
-            ))}
+          <div className="flex w-full gap-3">
+            <div className="flex max-w-xl flex-col gap-3">
+              <p className="text-2xl font-bold">Last week</p>
+              {weekSorted.map((user, index) => (
+                <div
+                  className={`flex items-center gap-3 rounded-md ${index == 0 ? "bg-yellow-500" : index == 1 ? "bg-gray-400" : index == 2 ? "bg-amber-600" : "bg-gray-200"} p-4`}
+                >
+                  <p className="pr-5 pl-2 font-bold">{index + 1}</p>
+                  <p className="font-bold">{user.name}</p>
+                  <div className="grow"></div>
+                  <p>
+                    {user.count} review{user.count > 1 && "s"}
+                  </p>
+                </div>
+              ))}
+            </div>
+            <div className="flex max-w-xl flex-col gap-3">
+              <p className="text-2xl font-bold">All time</p>
+              {alltimeSorted.map((user, index) => (
+                <div
+                  className={`flex items-center gap-3 rounded-md ${index == 0 ? "bg-yellow-500" : index == 1 ? "bg-gray-400" : index == 2 ? "bg-amber-600" : "bg-gray-200"} p-4`}
+                >
+                  <p className="pr-5 pl-2 font-bold">{index + 1}</p>
+                  <p className="font-bold">{user.name}</p>
+                  <div className="grow"></div>
+                  <p>
+                    {user.count} review{user.count > 1 && "s"}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
