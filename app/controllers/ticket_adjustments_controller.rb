4915cff5953da87070674cefe92d4b45a0900d8c
polypixeldev <me@sfernandez.dev>
2026-03-13 09:12:24 -0400

    Add flash to ticket adjustments

--- app/controllers/ticket_adjustments_controller.rb
+++ app/controllers/ticket_adjustments_controller.rb
@@ -1,9 +1,13 @@
 class TicketAdjustmentsController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   skip_after_action :verify_authorized
   before_action :signed_in_admin
 
   def create
     TicketAdjustment.create!(adjustment_params.merge(user_id: params[:user_id].to_i))
+
+    flash[:notice] = "#{adjustment_params[:amount].positive? ? "Added" : "Removed"} #{pluralize(adjustment_params[:amount].abs, "ticket")}"
 
     redirect_back_or_to user_path(params[:user_id])
   end
@@ -11,6 +15,8 @@ class TicketAdjustmentsController < ApplicationController
   def destroy
     adjustment = TicketAdjustment.find(params[:id])
     adjustment.destroy!
+
+    flash[:notice] = "Removed ticket adjustment for #{pluralize(adjustment.amount, "ticket")}"
 
     redirect_back_or_to user_path(adjustment.user)
   end
