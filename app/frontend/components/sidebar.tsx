08e9e04ff07d66b754ad920593e7aa7d57664370
Arav Narula <arav@hackclub.com>
2026-03-17 13:48:43 -0400

    invite -> Referrals

--- app/frontend/components/sidebar.tsx
+++ app/frontend/components/sidebar.tsx
@@ -149,7 +149,7 @@ export default function Sidebar() {
             <SidebarLink link="/explore" name="Gallery" icon={compassIcon} />
             <SidebarLink link="/shop" name="Shop" icon={shopIcon} />
             {props.user.referral_program_active && (
-              <SidebarLink link="/invite" name="Invite" icon={inviteIcon} />
+              <SidebarLink link="/invite" name="Referrals" icon={inviteIcon} />
             )}
             {(props.user.role === "admin" ||
               props.user.role === "reviewer") && (
