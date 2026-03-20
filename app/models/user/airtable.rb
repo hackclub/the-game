dc7348bb2668a305f8b6809eaedf8b82b191d122
phthallo <annabelquach13@gmail.com>
2026-03-20 14:48:59 -0400

    fix field names

--- app/models/user/airtable.rb
+++ app/models/user/airtable.rb
@@ -6,7 +6,7 @@ class User
     field :first_name, "First Name"
     field :verification_status, "Verification Status"
     field :hackatime_linked, "Hackatime Linked?"
-    field :first_project_created_at, "Loops - firstProjectCreatedAt"
-    field :first_project_ship_at, "Loops - firstProjectShipAt"
+    field :first_project_created_at, "Loops - hctgFirstProjectCreatedAt"
+    field :first_project_ship_at, "Loops - hctgFirstProjectShipAt"
   end
 end
