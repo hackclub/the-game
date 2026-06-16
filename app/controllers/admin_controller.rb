d158a6b1202f19d63697ae222fed0dfca9ec915e
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-06-15 23:12:55 -0400

    refactor: deduplicate budget balance calculation by reusing User#balance

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -327,14 +327,7 @@ class AdminController < ApplicationController
     users = User.where.not(id: excluded_user_ids)
       .includes(:approved_reviews, :purchases, :ticket_adjustments, :incoming_ticket_transfers, :outgoing_ticket_transfers)
 
-    total_user_balance_tickets = users.sum do |u|
-      revenue = ((u.approved_reviews.reduce(0) { |acc, r| acc + (r.approved_seconds || 0) }) / 3600.0).floor
-      expenses = u.purchases.reduce(0) { |acc, p| acc + p.amount_paid }
-      adjustments = u.ticket_adjustments.reduce(0) { |acc, a| acc + a.amount }
-      incoming = u.incoming_ticket_transfers.select { |t| t.aasm_state == "approved" }.reduce(0) { |acc, t| acc + t.amount }
-      outgoing = u.outgoing_ticket_transfers.select { |t| t.aasm_state != "rejected" }.reduce(0) { |acc, t| acc + t.amount }
-      revenue + adjustments - expenses + incoming - outgoing
-    end
+    total_user_balance_tickets = users.sum { |u| u.balance }
 
     render inertia: "admin/budget", props: {
       items:,
